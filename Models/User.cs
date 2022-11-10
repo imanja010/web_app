a78a94cd8109b817aacee222ef0c2285d101c440
kurspirot03 <kurs.pirot.03@gmail.com>
2022-11-10 20:17:31 +0100

    Username
    Added users model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace web_app.Models
+{
+    public class User
+    {
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
