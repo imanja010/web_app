d670334fd14191889fdf17ff66492d2d250a59a3
kurspirot02 <kurs.pirot.02@gmail.com>
2022-11-10 20:16:49 +0100

    Favourites
    Favourites

---
+++ Controllers/FavouritesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web_app.Models;
+
+namespace web_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FavouritesController : Controller
+    {
+        [HttpGet(Name = "GetFavourites")]
+        public Favourites Get()
+        {
+            return new Favourites()
+            {
+                Song = "TestSong",
+                Artist = "TestArtist",
+                Album = "TestAlbum"
+            };
+        }
+    }
+}
