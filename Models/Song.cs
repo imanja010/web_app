77ab7bccbbb7ffef17d4542eddd4b4d82e4c24a1
kurspirot04 <kurs.pirot.04@gmail.com>
2022-11-15 18:35:47 +0100

    Update Song.cs
    added song

--- Models/Song.cs
+++ Models/Song.cs
@@ -2,5 +2,9 @@
 {
     public class Song
     {
+        public string SongName { get; set; }
+        public string SongArtist { get; set; }
+        public string SongDescription { get; set; }
+        public string SongAlbum { get; set; }
     }
 }
