20734b59d76b70e74089172c46ec24708053ed8b
kurspirot02 <kurs.pirot.02@gmail.com>
2022-11-29 20:15:29 +0100

    Petronije
    stvari

--- Services/SongService.cs
+++ Services/SongService.cs
@@ -16,6 +16,24 @@ namespace web_app.Services
 
 
         }
+
+        public void AddSong(Song song)
+        {
+            songList.Add(song);
+        }
+
+        internal void Remove(long id)
+        {
+            var ItemForRemoval = songList.FirstOrDefault(x => x.Id == id);
+            if (ItemForRemoval != null) 
+            
+            {
+                songList.Remove(ItemForRemoval);
+            
+            }
+        }
+
+
         private List<Song> songList = new List<Song>
         {
             new Song()
@@ -44,6 +62,33 @@ namespace web_app.Services
                 SongName="Dead and bloated"
                 
             },
+             new Song()
+            {
+                Id = 4,
+                SongName = "Them bones",
+                SongArtist = "Alice in Chains",
+                SongDescription = "Najjaca pesma ikada, nasa najbolja umetnica je kreirala najjaca je",
+                SongAlbum = "Dirt"
+
+            },
+            new Song()
+            {
+                Id= 5,
+                SongAlbum="Voules Vouz",
+                SongArtist ="Abba",
+                SongName = "Gimme!Gimme!Gimme!",
+                SongDescription = "Volis li me kad sam mrtav pijan, volis li me kad pravi sam grubijan, volis li me kad okolo se ku*vam"
+            },
+            new Song()
+            {
+                Id = 6,
+                SongAlbum="No Need To Argue",
+                SongArtist="The Cranberries",
+                SongDescription="A IM SMELLING LIKE THE ROSES SOMEBODY GAVE ME ON MY BIRTHDAY DEATHBED",
+                SongName="Zombie"
+
+            },
+
         };
     }
 }
